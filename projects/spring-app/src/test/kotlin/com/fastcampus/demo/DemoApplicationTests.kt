e227926d533770c58f03449753a6c030872490c6
junoyoon <junoyoon@gmail.com>
2023-06-26 17:53:10 +0900

    fix

--- projects/spring-app/src/test/kotlin/com/fastcampus/demo/DemoApplicationTests.kt
+++ projects/spring-app/src/test/kotlin/com/fastcampus/demo/DemoApplicationTests.kt
@@ -1,13 +1,36 @@
 package com.fastcampus.demo
 
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class DemoApplicationTests {
+class DemoApplicationTests(
+    @Value("\${spring.datasource.url}")
+    val dataSourceUrl: String
+) {
 
     @Test
     fun contextLoads() {
         // empty
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "SLOW_TEST_ENABLED", matches = "true")
+    fun slowTest() {
+        println("************************************")
+        println("slow test running")
+        Thread.sleep(1000 * 30)
+        println("slow test finished")
+        println("************************************")
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "SPRING_PROFILES_ACTIVE", matches = ".*")
+    fun dataSourceUrlTest() {
+        println("************************************")
+        println("spring.datasource.url $dataSourceUrl")
+        println("************************************")
     }
 }
