292cb218c0790e0df4390a38ef904dccc659e83e
sunwondx <104117486+sunwondx@users.noreply.github.com>
2023-09-26 18:50:18 +0900

    Update build.gradle.kts

--- projects/spring-app/build.gradle.kts
+++ projects/spring-app/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.12"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    id("org.jlleitschuh.gradle.ktlint") version "11.3.2"
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.0"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     // jacoco
